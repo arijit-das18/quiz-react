46e8382558a4e1d6a249b0df7947b830dc7ca489
Sunit Khatua <sunitkhatua00@gmail.com>
2023-02-24 14:31:20 +0530

    file uploaded

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './components/Card/Card'
+
+const App = () => {
+  return (
+    <div>
+
+        <>
+          <Card/>
+        </>
+
+    </div>
+  )
+}
+
+export default App
